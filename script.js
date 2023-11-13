8a3bc9e48d0b4ea4fd34de98e8dcd33fefa2af70
GadingAgani <149600002+iLuviee@users.noreply.github.com>
2023-11-13 09:09:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+AOS.init();
+
+//Navbar Links Active On Scroll
+$(document).ready(function () {
+  $("body").scrollspy({
+    target: "#navbar",
+  });
+});
+//Navbar Links Active On Click
+const ul = document.querySelector(".navbar-nav");
+ul.addEventListener("click", (e) => {
+  if (e.target.className == "nav-link") {
+    ul.querySelectorAll("li a").forEach((el) => el.classList.remove("active"));
+    e.target.classList.add("active");
+  }
+});
